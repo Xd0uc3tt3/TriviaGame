c4c3f1f130d69d5eeec450c2adce29318bd557f7
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2025-12-03 12:02:00 -0400

    Started run game method
    Just has the starting menu

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace TriviaGame
     {
         static void Main(string[] args)
         {
+            RunGame();
+        }
+
+        static void RunGame()
+        {
+            Console.WriteLine("Xander's Trivia Game");
+            Console.WriteLine("");
+            Console.WriteLine("What is your name?: ");
+            string playername = Console.ReadLine();
+
+            bool IsPlaying = true;
+
+            while (IsPlaying)
+            {
+                //
+            }
         }
     }
 }
